8968e9b966452e84c2f34c1299e04ad5555728fd
poonamak <patilpoonam211@gmail.com>
2017-10-11 16:12:23 +0530

    Feature-One 26th commit.

--- src/Travel.Connectors.Hotel/Controllers/Values.cs
+++ src/Travel.Connectors.Hotel/Controllers/Values.cs
@@ -15,7 +15,7 @@ namespace Travel.Connectors.Hotel.Controllers
         public IEnumerable<string> Get(string test)
         {
             //testData();
-            return new string[] { "value 125", "value 225", "value 325"};
+            return new string[] { "value 126", "value 226", "value 326"};
         }
 
         // GET api/values/5
